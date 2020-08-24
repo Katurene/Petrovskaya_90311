789e96d5e430d4d6cf5108a751383609af24e1c0
Katurene <katurene7@gmail.com>
2020-08-24 14:48:23 +0300

    Создаю контроллер с именем Home

---
+++ Petrovskaya_90311/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Petrovskaya_90311.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
