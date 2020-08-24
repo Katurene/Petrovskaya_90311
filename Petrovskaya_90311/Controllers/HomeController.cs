70b010c99d4451a3062af666402441b092956bf8
Katurene <katurene7@gmail.com>
2020-08-24 16:46:21 +0300

    Формирование списка Завершение лаб2

--- Petrovskaya_90311/Controllers/HomeController.cs
+++ Petrovskaya_90311/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Petrovskaya_90311.Controllers
 {
@@ -11,7 +12,27 @@ namespace Petrovskaya_90311.Controllers
         public IActionResult Index()
         {
             ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
+            
+        }
+
+        public class ListDemo
+        {
+            public int ListItemValue { get; set; }
+            public string ListItemText { get; set; }
+        }
+
+        private List<ListDemo> _listDemo;
+
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
     }
 }
