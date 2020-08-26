14800fa9a50b8f34e606809b62a0c60eb36fb42e
Katurene <katurene7@gmail.com>
2020-08-26 13:58:55 +0300

    П.5,2 Инициализация списка элементов меню

---
+++ Petrovskaya_90311/Components/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Petrovskaya_90311.Models;
+
+namespace Petrovskaya_90311.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        // Инициализация списка элементов меню
+        private List<MenuItem> _menuItems = new List<MenuItem>
+        {
+            new MenuItem{ Controller="Home", Action="Index", Text="Lab 2"},
+            new MenuItem{ Controller="Product", Action="Index", Text="Каталог"},
+            new MenuItem{ IsPage=true, Area="Admin", Page="/Index", Text="Администрирование"}
+        };
+    }
+}
