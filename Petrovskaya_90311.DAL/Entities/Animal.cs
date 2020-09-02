978c505ca171391c3c769e0ce44603f7a814250f
Katurene <katurene7@gmail.com>
2020-09-02 15:21:43 +0300

    Созданы классы предметной области

---
+++ Petrovskaya_90311.DAL/Entities/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Petrovskaya_90311.DAL.Entities
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; } // id животного
+        public string AnimalName { get; set; } // название животного
+        public string Description { get; set; } // описание 
+        public int Age { get; set; } // возраст
+        public string Image { get; set; } // имя файла изображения
+
+        // Навигационные свойства
+        /// <summary>
+        /// группа блюд (например, супы, напитки и т.д.)
+        /// </summary>
+        public int AnimalGroupId { get; set; }
+        public AnimalGroup Group { get; set; }
+    }
+}
---
+++ Petrovskaya_90311.DAL/Entities/AnimalGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Petrovskaya_90311.DAL.Entities
+{
+    public class AnimalGroup
+    {
+        public int AnimalGroupId { get; set; }
+        public string GroupName { get; set; }
+        /// <summary>
+        /// Навигационное свойство 1-ко-многим
+        /// </summary>
+        public List<Animal> Animals { get; set; }
+    }
+}
