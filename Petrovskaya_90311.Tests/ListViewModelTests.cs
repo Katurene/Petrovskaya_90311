7b56aa194fdc3eb28302f6dc029b274d479e5560
Katurene <katurene7@gmail.com>
2020-09-03 17:45:39 +0300

    Создали класс ListViewModelTests

---
+++ Petrovskaya_90311.Tests/ListViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using Petrovskaya_90311.DAL.Entities;
+using Petrovskaya_90311.Models;
+using Xunit;
+
+namespace Petrovskaya_90311.Tests
+{
+    public class ListViewModelTests
+    {
+        [Fact]
+        public void ListViewModelCountsPages()
+        {
+            // Act
+            var model = ListViewModel<Animal>
+            .GetModel(TestData.GetDishesList(), 1, 3);
+            // Assert
+            Assert.Equal(2, model.TotalPages);
+        }
+
+        [Theory]
+        [MemberData(memberName: nameof(TestData.Params),
+        MemberType = typeof(TestData))]
+
+        public void ListViewModelSelectsCorrectQty(int page, int qty, int id)
+        {
+            // Act
+            var model = ListViewModel<Animal>
+            .GetModel(TestData.GetDishesList(), page, 3);
+            // Assert
+            Assert.Equal(qty, model.Count);
+        }
+        [Theory]
+        [MemberData(memberName: nameof(TestData.Params),
+        MemberType = typeof(TestData))]
+
+        public void ListViewModelHasCorrectData(int page, int qty, int id)
+        {
+            // Act
+            var model = ListViewModel<Animal>
+            .GetModel(TestData.GetDishesList(), page, 3);
+            // Assert
+            Assert.Equal(id, model[0].AnimalId);
+        }
+    }
+}
