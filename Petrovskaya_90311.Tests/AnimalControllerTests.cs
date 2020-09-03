e4bffea42db724b2424d710985defb0860974cc0
Katurene <katurene7@gmail.com>
2020-09-03 17:41:46 +0300

    Создали TestData и изменили AnimalControllerTests Тесты работают

--- Petrovskaya_90311.Tests/AnimalControllerTests.cs
+++ Petrovskaya_90311.Tests/AnimalControllerTests.cs
@@ -9,21 +9,12 @@ namespace Petrovskaya_90311.Tests
     public class AnimalControllerTests
     {
         [Theory]
-        [InlineData(1, 3, 1)] // 1-я страница, кол. объектов 3, id первого объекта 1
-        [InlineData(2, 2, 4)] // 2-я страница, кол. объектов 2, id первого объекта 4
-        
+        [MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
         public void ControllerGetsProperPage(int page, int qty, int id)
         {
             // Arrange
             var controller = new AnimalController();
-            controller._animals = new List<Animal>
-            {
-                new Animal{ AnimalId=1},
-                new Animal{ AnimalId=2},
-                new Animal{ AnimalId=3},
-                new Animal{ AnimalId=4},
-                new Animal{ AnimalId=5}
-            };
+            controller._animals = TestData.GetDishesList();
             // Act
             var result = controller.Index(page) as ViewResult;
             var model = result?.Model as List<Animal>;
---
+++ Petrovskaya_90311.Tests/TestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Petrovskaya_90311.DAL.Entities;
+
+namespace Petrovskaya_90311.Tests
+{
+    public class TestData
+    {
+        public static List<Animal> GetDishesList()
+        {
+            return new List<Animal>
+            {
+                new Animal{ AnimalId=1},
+                new Animal{ AnimalId=2},
+                new Animal{ AnimalId=3},
+                new Animal{ AnimalId=4},
+                new Animal{ AnimalId=5}
+            };
+        }
+
+        public static IEnumerable<object[]> Params()
+        {
+            // 1-я страница, кол. объектов 3, id первого объекта 1
+            yield return new object[] { 1, 3, 1 };
+            // 2-я страница, кол. объектов 2, id первого объекта 4
+            yield return new object[] { 2, 2, 4 };
+        }
+    }
+}
