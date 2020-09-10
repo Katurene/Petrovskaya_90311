a14ed7f5a9b137b8e2f4156e015269bdaad22cbe
Katurene <katurene7@gmail.com>
2020-09-10 09:44:40 +0300

    В класс ApplicationDbContext добавили сущности  предметной области

--- Petrovskaya_90311.DAL/Data/ApplicationDbContext.cs
+++ Petrovskaya_90311.DAL/Data/ApplicationDbContext.cs
@@ -9,6 +9,9 @@ namespace Petrovskaya_90311.DAL.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<AnimalGroup> AnimalGroups { get; set; }
+
         public
         ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
