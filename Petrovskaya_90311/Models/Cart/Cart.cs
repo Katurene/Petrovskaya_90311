f83be10c1fbc3a43797219f2db020f35b18c1167
Katurene <katurene7@gmail.com>
2020-09-10 23:19:41 +0300

    Сделана модель корзины заказов

---
+++ Petrovskaya_90311/Models/Cart/Cart.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Petrovskaya_90311.DAL.Entities;
+
+namespace Petrovskaya_90311.Models.Cart
+{
+    public class Cart
+    {
+        public Dictionary<int, CartItem> Items { get; set; }
+        public Cart()
+        {
+            Items = new Dictionary<int, CartItem>();
+        }
+        /// <summary>
+        /// Количество объектов в корзине
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return Items.Sum(item => item.Value.Quantity);
+            }
+        }
+        /// <summary>
+        /// Количество калорий
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                return Items.Sum(item => item.Value.Quantity *
+                item.Value.Animal.Age);
+            }
+        }
+        /// <summary>
+        /// Добавление в корзину
+        /// </summary>
+        /// <param name="animal">добавляемый объект</param>
+        public void AddToCart(Animal animal)
+        {
+            // если объект есть в корзине
+            // то увеличить количество
+            if (Items.ContainsKey(animal.AnimalId))
+                Items[animal.AnimalId].Quantity++;
+            // иначе - добавить объект в корзину
+            else
+                Items.Add(animal.AnimalId, new CartItem
+                {
+                    Animal = animal,
+                    Quantity = 1
+                });
+        }
+        /// <summary>
+        /// Удалить объект из корзины
+        /// </summary>
+        /// <param name="id">id удаляемого объекта</param>
+        public void RemoveFromCart(int id)
+        {
+            Items.Remove(id);
+        }
+        /// <summary>
+        /// Очистить корзину
+        /// </summary>
+        public void ClearAll()
+        {
+            Items.Clear();
+        }
+    }
+}
