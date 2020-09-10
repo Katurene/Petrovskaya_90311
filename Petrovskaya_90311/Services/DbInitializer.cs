ddfe8dede8f9a15b3a2e5663bbef539b41e832c9
Katurene <katurene7@gmail.com>
2020-09-10 10:08:00 +0300

    В классе DbInitializer добавлена инициализация данных сущностных классов

--- Petrovskaya_90311/Services/DbInitializer.cs
+++ Petrovskaya_90311/Services/DbInitializer.cs
@@ -16,6 +16,73 @@ namespace Petrovskaya_90311.Services
         {
             // создать БД, если она еще не создана
             context.Database.EnsureCreated();
+
+            //проверка наличия групп объектов
+            if (!context.AnimalGroups.Any())
+            {
+                context.AnimalGroups.AddRange(
+                new List<AnimalGroup>
+                {
+                    new AnimalGroup {GroupName="Попугаи"},
+                    new AnimalGroup {GroupName="Приматы"},
+                    new AnimalGroup {GroupName="Змеи"},
+                    new AnimalGroup {GroupName="Грызуны"},
+                    new AnimalGroup {GroupName="Кошки"}
+                });
+                await context.SaveChangesAsync();
+            }
+            // проверка наличия объектов
+            if (!context.Animals.Any())
+            {
+                context.Animals.AddRange(
+                new List<Animal>
+                {
+                new Animal {AnimalName="Попугай Ара",
+                    Description="Крупные попугаи с очень яркой окраской зелёных, " +
+                    "красных, голубых и жёлтых тонов ",
+                    Age =50, AnimalGroupId=1, Image="Ara.jpg" },
+                new Animal {AnimalName="Попугай корелла",
+                    Description="Средних размеров попугай, почти половину длины тела составляет длина хвоста." +
+                    "Вес тела от 80 до 150 грамм.",
+                    Age =15, AnimalGroupId=1, Image="Corella.jpg" },
+
+                new Animal {AnimalName="Мартышка",
+                    Description="Довольно небольшие обезьяны. Длина тела составляет от 30 до 100 сантиметров.",
+                    Age =10, AnimalGroupId=2, Image="Martishka.jpg" },
+                new Animal { AnimalName="Шимпанзе",
+                    Description="Род обезьян из семейства гоминидов.",
+                    Age =35, AnimalGroupId=2, Image="Shimpanze.jpg" },
+                new Animal {AnimalName="Павиан",
+                    Description="Эти обезьяны отличаются высокой агрессивностью, но при этом умело уживаются с людьми.",
+                    Age =15, AnimalGroupId=2, Image="Pavian.jpg" },
+
+                new Animal {AnimalName="Коралловый аспид",
+                    Description="Род ядовитых змей из семейства аспидов (Elapidae). Имеют яркую окраску с " +
+                    "характерными чёрными, красными и жёлтыми кольцами",
+                    Age =3, AnimalGroupId=3, Image="Aspid.jpg" },
+                new Animal {AnimalName="Черная мамба",
+                    Description="Ядовитая змея, распространённая в Африке. Длина тела может превышать 3 м",
+                    Age =5, AnimalGroupId=3, Image="Mamba.jpg" },
+                new Animal {AnimalName="Тигровый питон",
+                    Description="Крупная неядовитая змея. Длина тела может варьироваться от 1,5 до 4 м и более, в зависимости от пола",
+                    Age =15, AnimalGroupId=3, Image="Piton.jpg" },
+
+                new Animal {AnimalName="Сурок",
+                    Description="Крупные, весом в несколько килограммов, животные, обитающие в открытых " +
+                    "ландшафтах, в сооружаемых самостоятельно норах." ,
+                    Age =2, AnimalGroupId=4, Image="Surok.jpg" },
+                new Animal {AnimalName="Хомяк",
+                    Description="Размеры хомячков могут варьироваться от 5 до 37 см," +
+                    " а вес — от 45 до 700 граммов.",
+                    Age =1, AnimalGroupId=4, Image="Homyak.jpg" },
+
+                new Animal { AnimalId = 11, AnimalName="Бенгальская кошка",
+                    Description="Межродовой гибрид домашней кошки и бенгальской кошки ",
+                    Age =7, AnimalGroupId=5, Image="Koshka.jpg" }
+                });
+                await context.SaveChangesAsync();
+            }
+
             // проверка наличия ролей
             if (!context.Roles.Any())
             {
