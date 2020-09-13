696d8ffa9d48497507aeb1d661ef5af9a04456a7
Katurene <katurene7@gmail.com>
2020-09-13 13:08:39 +0300

    Описание моделей представления

---
+++ Petrovskaya_90311.Blazor/Data/ApiViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Petrovskaya_90311.Blazor.Data
+{
+    public class AnimalListViewModel
+    {
+        [JsonPropertyName("animalId")]
+        public int AnimalId { get; set; } // id 
+        [JsonPropertyName("animalName")]
+        public string AnimalName { get; set; } // название 
+    }
+
+    public class AnimalDetailsViewModel
+    {
+        [JsonPropertyName("animalName")]
+        public string AnimalName { get; set; } // название 
+        [JsonPropertyName("description")]
+        public string Description { get; set; } // описание 
+        [JsonPropertyName("age")]
+        public int Age { get; set; } // 
+        [JsonPropertyName("image")]
+        public string Image { get; set; } // имя файла изображения
+    }
+}
